9c9145e18b6ecf701babacfacda4643e4a9ad842
Ramzi <mohamed-ramzi.zerhouni@proton.me>
2024-12-02 21:44:41 +0100

    1000 particles

--- main.cpp
+++ main.cpp
@@ -1,30 +1,73 @@
 #include <raylib.h>
+#include <cstdlib>  // For rand() and srand()
+#include <ctime>    // For time()
+
+// Particle struct definition
+struct Particle {
+    Vector2 position;
+    Vector2 velocity;
+    Color color;
+};
 
 int main() {
-    // Set the screen width and height
-    int screenWidth = 800;
+    // Set up window
+    int screenWidth = 1280;
     int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "Particle System");
 
-    // Initialize the window
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
+    // Seed the random number generator
+    srand(static_cast<unsigned int>(time(0)));
 
-    // Set the frame rate to 60 FPS
+    // Number of particles
+    const int numParticles = 1000;
+
+    // Array of particles
+    Particle particles[numParticles];
+
+    // Initialize particles with random properties
+    for (int i = 0; i < numParticles; i++) {
+        particles[i].position = { (float)(rand() % screenWidth), (float)(rand() % screenHeight) };  // Random position
+        particles[i].velocity = { 5 };  // Random velocity (-2 to 2)
+        particles[i].color = Color{ (unsigned char)(rand() % 256), (unsigned char)(rand() % 256),
+                                    (unsigned char)(rand() % 256), 255 };  // Random color
+    }
+
+    // Set the frame rate
     SetTargetFPS(144);
 
     // Main game loop
     while (!WindowShouldClose()) {
+        // Update particle positions based on their velocities
+        for (int i = 0; i < numParticles; i++) {
+            particles[i].position.x += particles[i].velocity.x;
+            particles[i].position.y += particles[i].velocity.y;
+
+            // If the particle goes off the screen, reset it to a random position
+            if (particles[i].position.x > screenWidth || particles[i].position.x < 0) {
+                particles[i].position.x = rand() % screenWidth;
+            }
+            if (particles[i].position.y > screenHeight || particles[i].position.y < 0) {
+                particles[i].position.y = rand() % screenHeight;
+            }
+        }
+
         // Start drawing
         BeginDrawing();
+        ClearBackground(RAYWHITE);
 
-        ClearBackground(BLACK);
+        // Draw all particles
+        for (int i = 0; i < numParticles; i++) {
+            DrawCircleV(particles[i].position, 8.0f, particles[i].color);
+        }
 
-        DrawText("Welcome to Raylib!", 190, 200, 20, WHITE);
+        // Display the FPS in the top-left corner
+        DrawText(TextFormat("FPS: %i", GetFPS()), 10, 10, 20, DARKGRAY);
 
         // End drawing
         EndDrawing();
     }
 
-    // Close the window and OpenGL context
+    // Close window
     CloseWindow();
 
     return 0;
