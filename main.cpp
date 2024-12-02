fe11b1b7bf62bfbf56114103f80d13efba434c28
Ramzi <mohamed-ramzi.zerhouni@proton.me>
2024-12-02 22:01:53 +0100

    bounce and 35 FPS

--- main.cpp
+++ main.cpp
@@ -9,17 +9,19 @@ struct Particle {
     Color color;
 };
 
+#define MAX_PARTICLES_9_FPS 4e4 // It gives 9 FPS
+#define MAX_PARTICLES_35_FPS 1e4 // It gives 35 FPS
 int main() {
     // Set up window
-    int screenWidth = 1280;
-    int screenHeight = 600;
+    int screenWidth = 1440;
+    int screenHeight = 920;
     InitWindow(screenWidth, screenHeight, "Particle System");
 
     // Seed the random number generator
     srand(static_cast<unsigned int>(time(0)));
 
     // Number of particles
-    const int numParticles = 1000;
+    const int numParticles = MAX_PARTICLES_35_FPS;
 
     // Array of particles
     Particle particles[numParticles];
@@ -27,7 +29,7 @@ int main() {
     // Initialize particles with random properties
     for (int i = 0; i < numParticles; i++) {
         particles[i].position = { (float)(rand() % screenWidth), (float)(rand() % screenHeight) };  // Random position
-        particles[i].velocity = { 5 };  // Random velocity (-2 to 2)
+        particles[i].velocity = { (float)(rand() % 5 - 2), (float)(rand() % 5 - 2) };  // Random velocity (-2 to 2)
         particles[i].color = Color{ (unsigned char)(rand() % 256), (unsigned char)(rand() % 256),
                                     (unsigned char)(rand() % 256), 255 };  // Random color
     }
@@ -42,26 +44,26 @@ int main() {
             particles[i].position.x += particles[i].velocity.x;
             particles[i].position.y += particles[i].velocity.y;
 
-            // If the particle goes off the screen, reset it to a random position
-            if (particles[i].position.x > screenWidth || particles[i].position.x < 0) {
-                particles[i].position.x = rand() % screenWidth;
+            // Bounce off the edges of the screen (left, right, top, bottom)
+            if (particles[i].position.x >= screenWidth || particles[i].position.x <= 0) {
+                particles[i].velocity.x *= -1;  // Reverse horizontal velocity
             }
-            if (particles[i].position.y > screenHeight || particles[i].position.y < 0) {
-                particles[i].position.y = rand() % screenHeight;
+            if (particles[i].position.y >= screenHeight || particles[i].position.y <= 0) {
+                particles[i].velocity.y *= -1;  // Reverse vertical velocity
             }
         }
 
         // Start drawing
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
         // Draw all particles
         for (int i = 0; i < numParticles; i++) {
-            DrawCircleV(particles[i].position, 8.0f, particles[i].color);
+            DrawCircleV(particles[i].position, 5.0f, particles[i].color);
         }
 
         // Display the FPS in the top-left corner
-        DrawText(TextFormat("FPS: %i", GetFPS()), 10, 10, 20, DARKGRAY);
+        DrawText(TextFormat("FPS: %i", GetFPS()), 10, 10, 50, WHITE);
 
         // End drawing
         EndDrawing();
